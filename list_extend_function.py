b6a993781497c2f794e6bdcbf6710d930103c0e5
AaronTho <aamith@gmail.com>
2021-06-10 08:52:09 -0600

    extend function

--- list_extend_function.py
+++ list_extend_function.py
@@ -3,5 +3,20 @@ tags = [
     'development',
     'tutorials',
     'code',
-    'programming',
 ]
+
+# Nope
+
+tags[-1] = 'Programing'
+
+# In place
+tags.extend("Programming")
+
+new_tags = tags.append('Programming')
+
+# New List
+tags.extend(["Programming"])
+
+new_tags = tags + ['Programming']
+
+print(new_tags)
