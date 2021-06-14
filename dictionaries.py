c74fb5641f22595646258aa4d372d04663141e74
AaronTho <aamith@gmail.com>
2021-06-14 07:43:54 -0600

    added dictionaries and nested collections

--- dictionaries.py
+++ dictionaries.py
@@ -13,3 +13,19 @@ customer["birthdate"] = "Jan 1 1980"
 print(customer['name'])
 # get will return "none" instead of an error if the key is not in the dictionary
 print(customer.get("birthdate"))
+
+
+# Botegga Dictionary Notes
+
+players = {
+    "ss": "Correa",
+    "2b": "Altuva",
+    "3b": "Bregmans",
+    "DH": "Gattis",
+    "OF": "Springer",
+
+}
+
+second_base = players['2b']
+designated_hitter = players['DH']
+print(second_base)
