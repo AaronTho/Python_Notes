85c313fcfbe893a02f4c823049f96ef10ae2cf8f
AaronTho <aamith@gmail.com>
2021-06-23 07:20:51 -0600

    bottega list module

--- dictionary_view_objects.py
+++ dictionary_view_objects.py
@@ -1,11 +1,15 @@
+players = {
+    "ss": "Correa",
+    "2b": "Altuva",
+    "3b": "Bregmans",
+    "DH": "Gattis",
+    "OF": "Springer",
+
+}
+
 teams = {
     "astros": ["Altuve", "Corea", "Bregman"],
     "giants": ["Pence", "Posey", "Austin", "Dugger"],
     "angels": ["Trout", "Pujols"],
     'yankees': ['Judge', 'Stanton'],
 }
-
-featured_team = teams.get('mets', 'No featured team')
-# Creates a fallback response insteat of throwing an error
-
-print(featured_team)
---
+++ lists_bottega_lessons.py
@@ -0,0 +1,12 @@
+# A list creates a true data structure.
+
+users = ['Kristine', "Tiffany", 'Jordan']
+
+users.insert(1, 'Anthony')
+
+
+users.append('Ian')
+# Adds the end of the list
+
+
+print(users[2])
