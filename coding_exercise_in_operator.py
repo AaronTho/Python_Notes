4ca6cf80dfd82ec15348967a12efded751d9fcd5
AaronTho <aamith@gmail.com>
2021-07-02 07:05:33 -0600

    added in operator coding exercise

---
+++ coding_exercise_in_operator.py
@@ -0,0 +1,11 @@
+def value_in_string():
+    sentence = 'Python is the best!'
+    word = 'Python'
+
+    if word.lower() in sentence.lower():
+        print('The word is in the sentence')
+    else:
+        print('The word is not in the sentence')
+
+
+value_in_string()
---
+++ coding_exercise_ternary_operator.py
@@ -0,0 +1,5 @@
+language = "python"
+
+language_check = True if language == 'python' else False
+
+print(language_check)
