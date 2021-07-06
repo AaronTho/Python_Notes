c96a2d45b9647768cedc4521eb877700b17584c2
AaronTho <aamith@gmail.com>
2021-07-06 09:22:54 -0600

    added import libraries

---
+++ import_libraries.py
@@ -0,0 +1,22 @@
+def greeting(first, last):
+    return f'Hi {first} {last}'
+
+
+print(greeting('Aaron', 'Thompson'))
+
+"""
+# libs/helper.py file
+def greeting(first, last):
+    return f'Hi {first} {last}!'
+
+# main.py file
+import sys
+sys.path.insert(0, './libs')
+import helper
+
+def render():
+    print(helper.greeting('Kristine', 'Hudgens'))
+
+
+render()
+"""
