03952c216163d87bc567e6a471c641979790fdc2
AaronTho <aamith@gmail.com>
2021-07-12 15:46:27 -0600

    Added guessing game and random number game

---
+++ quiz_game.py
@@ -0,0 +1,43 @@
+print('Welcome to my quiz!')
+
+playing = input('Do you wan to play? ')
+
+if playing.lower() != 'yes':
+    quit()
+
+print("Okay! Let's play :)")
+score = 0
+
+answer = input("What does GPU stand for? ")
+
+if answer.lower() == 'graphics processing unit':
+    print('Correct!')
+    score += 1
+else:
+    print('Sorry, but you are very dumb. Our data suggests your mother smoked a lot.')
+
+answer = input("What does RAM stand for? ")
+
+if answer.lower() == 'random access memory':
+    print('Correct!')
+    score += 1
+else:
+    print('Sorry, but you are very dumb. Our data suggests your mother smoked a lot.')
+
+answer = input("What does PSU stand for? ")
+
+if answer.lower() == 'power supply unit':
+    print('Correct!')
+    score += 1
+else:
+    print('Sorry, but you are very dumb. Our data suggests your mother smoked a lot.')
+
+answer = input("What does LDS stand for? ")
+
+if answer.lower() == 'latter day saint':
+    print('Correct!')
+    score += 1
+else:
+    print('Sorry, but you are very dumb. Our data suggests your mother smoked a lot.')
+
+print("You got " + str((score / 4) * 100) + "%")
---
+++ random_number_game.py
@@ -0,0 +1,32 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+random_number = random.randint(0, top_of_range)
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("Chicken Dinner!")
+        break
+    else:
+        print("This is profoundly wrong.")
+
+print("Look at the big brain on Brad. You got it in", guesses, "guesses.")
