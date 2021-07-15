c3d37a8bf678006f2b5297bb43817ca29b50040f
AaronTho <aamith@gmail.com>
2021-07-14 22:00:50 -0600

    finished choose your own adventure

--- choose_your_own_adveture.py
+++ choose_your_own_adveture.py
@@ -19,11 +19,21 @@ if answer == "left":
 
 elif answer == "right":
     went_right = input(
-        "You enter a forest of impossibly tall trees disapearing into the clouds. Type walk to enter the woods or climb to climb a tree: ").lower()
+        "You enter a forest of impossibly tall trees which disappear into the clouds. Type walk to enter the woods or climb to climb a tree: ").lower()
 
     if went_right == "walk":
-        print()
+        in_the_woods = input(
+            "You walk through the forest. The air is heavy and lush and you hear singing in the wind. Type 'follow' to find source of the singing and 'onward' to continue deeper into the woods").lower()
+        if in_the_woods == "follow":
+            print("You follow the huanting sound of singing to a green sun-soaked meadow hidden deep in the trees. Frogs eat you to death and you die dead.It's so lame.")
+        elif in_the_woods == "onward":
+            print("You force yourself to ignore the pull of the forest singing and push through the forest until you find a 7-11 built into the trunk of one of the massive trees. You order a Slurpee and win the game.")
+            quit()
+        else:
+            print("Not a valid option. You explode and die a fool.")
+
     elif went_right == "climb":
-        print()
+        print("You slowly climb the biggest tree you see, picking your way over the massive chunks of bark. At the top a bird looks at you and says, 'A frog is about to eat you, you never should have come up here.' She's right. You die.")
+        quit()
 else:
     print("Not a valid option. You explode.")
