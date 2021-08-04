47868c9898f579c5044a64b3986f794988a059d9
AaronTho <aamith@gmail.com>
2021-08-04 07:31:59 -0600

    updated custom interator class

--- build_a_custom_iterator_class.py
+++ build_a_custom_iterator_class.py
@@ -1,3 +1,22 @@
+class Lineup:
+    def __init__(self, players):
+        self.players = players
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n < (len(self.players) - 1):
+            player = self.players[self.n]
+            self.n += 1
+            return player
+        elif self.n == (len(self.players) - 1):
+            player = self.players[self.n]
+            self.n = 0
+            return player
+
+
 astros = [
     'Springer',
     'Bregman',
@@ -9,3 +28,21 @@ astros = [
     'Davis',
     'Tucker'
 ]
+
+astros_lineup = Lineup(astros)
+process = iter(astros_lineup)
+
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
+print(next(process))
